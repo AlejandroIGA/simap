7cbe3923c13dcab4a07a5e2e787351dc250014b6
Leomont07 <monteroleon52@gmail.com>
2024-03-19 16:16:22 -0600

    Login-Google Web

--- web/src/conf.js
+++ web/src/conf.js
@@ -1,6 +1,6 @@
 const conf = {
-  url: 'http://10.13.13.99:80/simap/webservice/back',
-  paypalGateway: 'http://10.13.13.99:3000',
+  url: 'http://192.168.216.81:80/simap/webservice/back',
+  paypalGateway: 'http://192.168.216.81:3000/paypal',
 };
 
 export default conf;

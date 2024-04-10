888ec54972f16d47cd682924c431c4d1a9c9b83a
Alejandro Infante <alejandroinfante.ga@gmail.com>
2024-04-10 14:03:13 -0600

    correccion inicio móvil

--- web/src/conf.js
+++ web/src/conf.js
@@ -1,6 +1,6 @@
 const conf = {
   url: 'http://dtai.uteq.edu.mx/~aleinf214/practicas/simapApp/webservice/back',
-  url2: 'http://192.168.0.7:80/simap/webservice/back',
+  url2: 'http://10.13.2.237:80/simap/webservice/back',
   paypalGateway: 'http://192.168.0.6:3000/paypalWeb',
 };
 
